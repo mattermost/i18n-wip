61f6b9fd79037cdfecbb56cda6b4719729b0a37c
Elisabeth Kulzer <elikul@elikul.de>
2021-01-05 18:46:41 +0100

    Rm circleci

--- .circleci/mattermost-webhook.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sed -i "s/SED_PROJECT/$1/g" /home/circleci/project/.circleci/webhook-data.json
-curl -i -X POST -H 'Content-Type: application/json' \
-     -d @/home/circleci/project/.circleci/webhook-data.json $2
