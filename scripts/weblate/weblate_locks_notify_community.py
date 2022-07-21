edb353b074409c47953055e1fe76ac43c1405089
Tom De Moor <tom@controlaltdieliet.be>
2022-07-21 16:48:49 +0200

    added Focalboard and Playbooks

--- scripts/weblate/weblate_locks_notify_community.py
+++ scripts/weblate/weblate_locks_notify_community.py
@@ -7,24 +7,26 @@ import json
 import requests
 
 projects={'mobile':{},'server':{},'webapp':{},'desktop':{},'glossary':{}}
-projects['mobile']['shipped']='mattermost-mobile_master'
-projects['server']['shipped']='mattermost-server_master'
-projects['webapp']['shipped']='mattermost-webapp_master'
-projects['desktop']['shipped']='mattermost-desktop'
-projects['glossary']['shipped']='glossary'
+projects['mobile']='mattermost/mattermost-mobile_master'
+projects['server']='mattermost/mattermost-server_master'
+projects['webapp']='mattermost/mattermost-webapp_master'
+projects['desktop']='mattermost/mattermost-desktop'
+projects['focalboard']='focalboard/webapp'
+projects['playbooks']='playbooks/webapp'
+projects['glossary']='mattermost/glossary'
 
 w=wlc.Weblate(apikey.myAPIKey)
 current_locks={}
 previous_locks= json.load(open("locks.txt"))
 
 for project in projects:
-  current_lock=w.get('https://translate.mattermost.com/api/components/mattermost/'+projects[project]['shipped']+'/lock/')
-  current_locks[projects[project]['shipped']]=current_lock['locked']
-  if (previous_locks[projects[project]['shipped']]!=current_lock['locked']):
+  current_lock=w.get('https://translate.mattermost.com/api/components/'+projects[project]+'/lock/')
+  current_locks[projects[project]]=current_lock['locked']
+  if (previous_locks[projects[project]]!=current_lock['locked']):
     print("LOCKED/UNLOCKED")
-    values = '{ "text": "# Shipped Languages '+projects[project]['shipped']+' is unlocked now in Weblate "}'
+    values = '{ "text": "# Shipped Languages '+projects[project]+' is unlocked now in Weblate "}'
     if current_lock['locked']==True:
-      values = '{ "text": "# Shipped Languages '+projects[project]['shipped']+' is locked now in Weblate "}'
+      values = '{ "text": "# Shipped Languages '+projects[project]+' is locked now in Weblate "}'
     try:
       headers = {'Content-Type': 'application/json',}
       print(values)
