903df54ff17607edd5bad114d32f12848565ea5e
Tom De Moor <tom@controlaltdieliet.bewq>
2022-08-16 16:49:45 +0000

    Adding PR review owners to notifications sent to the Mattermost i18n-channel

--- scripts/weblate/weblate_locks_notify_community.py
+++ scripts/weblate/weblate_locks_notify_community.py
@@ -17,18 +17,28 @@ projects['focalboard']='focalboard/webapp'
 projects['playbooks']='playbooks/webapp'
 projects['glossary']='mattermost/glossary'
 
-w=wlc.Weblate(apikey.myAPIKey)
+mention={}
+mention['mobile']='cc: @guillermo.vaya,@zef.hemel'
+mention['server']='cc: @guillermo.vaya,@zef.hemel'
+mention['webapp']='cc: @guillermo.vaya,@zef.hemel'
+mention['focalboard']='cc: @jesus.espino,@scott.bishel'
+mention['playbooks']='cc: @jesse.hallam,@caleb.roseland'
+mention['glossary']=''
+
+w=wlc.Weblate(key=apikey.myAPIKey)
 current_locks={}
-previous_locks= json.load(open("locks.txt"))
+previous_locks= json.load(open("/home/tom/scripts/mattermost/locks.txt"))
+
+
 
 for project in projects:
   current_lock=w.get('https://translate.mattermost.com/api/components/'+projects[project]+'/lock/')
   current_locks[projects[project]]=current_lock['locked']
   if (previous_locks[projects[project]]!=current_lock['locked']):
     print("LOCKED/UNLOCKED")
-    values = '{ "text": "# Shipped Languages '+projects[project]+' is unlocked now in Weblate "}'
+    values = '{ "text": "# Shipped Languages '+projects[project]+' is unlocked now in Weblate '+mention[project]+' "}'
     if current_lock['locked']==True:
-      values = '{ "text": "# Shipped Languages '+projects[project]+' is locked now in Weblate "}'
+      values = '{ "text": "# Shipped Languages '+projects[project]+' is locked now in Weblate '+mention[project]+' "}'
     try:
       headers = {'Content-Type': 'application/json',}
       print(values)
@@ -40,4 +50,4 @@ for project in projects:
         print(f'Other error occurred while notifying Mattermost-channel: {err.message} {err.args}')
     else:
         print('Success!')
-json.dump(current_locks,open("locks.txt",'w'))
+json.dump(current_locks,open("/home/tom/scripts/mattermost/locks.txt",'w'))
