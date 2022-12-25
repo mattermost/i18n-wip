e3093bbafbd8fabb64c389112964e5f8161b682b
Tom De Moor <tom@controlaltdieliet.be>
2022-12-25 10:38:53 +0100

    adding reminder function of component remains locked for longer thn one week

--- scripts/weblate/weblate_locks_notify_community.py
+++ scripts/weblate/weblate_locks_notify_community.py
@@ -6,10 +6,11 @@ import apikey
 import json
 import requests
 from requests.exceptions import HTTPError
-
+from datetime import datetime
+import datetime as DT
 
 projects={'mobile':{},'server':{},'webapp':{},'desktop':{},'glossary':{}}
-projects['mobile']='mattermost/mattermost-mobile_master'
+projects['mobile']='mattermost/mattermost-mobile-v2'
 projects['server']='mattermost/mattermost-server_master'
 projects['webapp']='mattermost/mattermost-webapp_master'
 projects['desktop']='mattermost/mattermost-desktop'
@@ -18,31 +19,26 @@ projects['playbooks']='playbooks/webapp'
 projects['glossary']='mattermost/glossary'
 
 mention={}
-mention['mobile']='cc: @guillermo.vaya,@pantelis.vratsalis'
-mention['server']='cc: @guillermo.vaya,@pantelis.vratsalis'
-mention['webapp']='cc: @guillermo.vaya,@pantelis.vratsalis'
+mention['mobile']='cc: @guillermo.vaya , @pantelis.vratsalis'
+mention['server']='cc: @guillermo.vaya , @pantelis.vratsalis'
+mention['webapp']='cc: @guillermo.vaya , @pantelis.vratsalis'
 mention['desktop']='cc: @devin.binnie'
-mention['focalboard']='cc: @jesus.espino,@scott.bishel'
-mention['playbooks']='cc: @jesse.hallam,@caleb.roseland'
+mention['focalboard']='cc: @jesus.espino , @scott.bishel'
+mention['playbooks']='cc: @jesse.hallam , @caleb.roseland'
 mention['glossary']=''
 
 w=wlc.Weblate(key=apikey.myAPIKey)
 current_locks={}
 previous_locks= json.load(open("locks.txt"))
 
-
-
-for project in projects:
-  current_lock=w.get('https://translate.mattermost.com/api/components/'+projects[project]+'/lock/')
-  current_locks[projects[project]]=current_lock['locked']
-  if (previous_locks[projects[project]]!=current_lock['locked']):
-    print("LOCKED/UNLOCKED")
-    values = '{ "text": "#### Shipped Languages '+projects[project]+' is unlocked now in Weblate '+mention[project]+' "}'
-    if current_lock['locked']==True:
-      values = '{ "text": "##### Shipped Languages '+projects[project]+' is locked now in Weblate '+mention[project]+' "}'
+def notificateChannel(project,current_lock):
+  values = '{ "text": "#### Shipped Languages '+projects[project]+' is unlocked now in Weblate '+mention[project]+' "}'
+  if current_lock['locked']==True:
+    values = '{ "text": "##### Shipped Languages '+projects[project]+' is locked now in Weblate '+mention[project]+' "}'
     try:
       headers = {'Content-Type': 'application/json',}
-      print(values)
+      current_locks[projects[project]]=current_lock['locked']
+      current_locks[projects[project]+"-channel_notified"]=now
       response = requests.post(apikey.communityWebhook,headers=headers,data=values)
       response.raise_for_status()
     except HTTPError as http_err:
@@ -51,3 +47,17 @@ for project in projects:
         print(f'Other error occurred while notifying Mattermost-channel: {err.message} {err.args}')
     else:
         print('Success!')
+
+for project in projects:
+  now=datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+  current_lock=w.get('https://translate.mattermost.com/api/components/'+projects[project]+'/lock/')
+  if (previous_locks[projects[project]]!=current_lock):
+    notificateChannel(project,current_lock)
+    current_locks[projects[project]+"-channel_notified"]=now
+  else:
+    previous_notification = datetime.strptime(previous_locks[projects[project]+"-channel_notified"], '%Y-%m-%d, %H:%M:%S')
+    if (datetime.now()-DT.timedelta(days=7))>previous_notification and current_lock:
+      print("reminder for "+str(projects[project]))
+      notificateChannel(project,current_lock)
+      current_locks[projects[project]+"-channel_notified"]=now
+json.dump(current_locks,open("locks.txt",'w'))
