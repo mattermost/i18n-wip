084ab02a0d92e874edcf5e1df09222ee49b77555
Tom De Moor <github3@controlaltdieliet.be>
2022-12-28 21:44:28 +0100

    Update weblate_locks_notify_community.py

--- scripts/weblate/weblate_locks_notify_community.py
+++ scripts/weblate/weblate_locks_notify_community.py
@@ -35,38 +35,34 @@ def notificateChannel(project,current_lock):
   values = '{ "text": "#### Shipped Languages '+projects[project]+' is unlocked now in Weblate '+mention[project]+' "}'
   if current_lock['locked']==True:
     values = '{ "text": "##### Shipped Languages '+projects[project]+' is locked now in Weblate '+mention[project]+' "}'
-  current_locks[projects[project]]=current_lock['locked']
-  if (previous_locks[projects[project]]!=current_lock['locked']):
-    print("LOCKED/UNLOCKED")
-    values = '{ "text": "#### Shipped Languages '+projects[project]+' is unlocked now in Weblate '+mention[project]+' "}'
-    if current_lock['locked']==True:
-      values = '{ "text": "##### Shipped Languages '+projects[project]+' is locked now in Weblate '+mention[project]+' "}'
-    try:
-      headers = {'Content-Type': 'application/json',}
-      current_locks[projects[project]]=current_lock['locked']
-      current_locks[projects[project]+"-channel_notified"]=now
-      response = requests.post(apikey.communityWebhook,headers=headers,data=values)
-      response.raise_for_status()
-    except HTTPError as http_err:
-        print(f'HTTP error occurred while notifying Mattermost-channel: {http_err}')
-    except Exception as err:
-        print(f'Other error occurred while notifying Mattermost-channel: {err.message} {err.args}')
-    else:
-        print('Success!')
+  try:
+    headers = {'Content-Type': 'application/json',}
+    current_locks[projects[project]]=current_lock
+    current_locks[projects[project]+"-channel_notified"]=now
+    print("locked sent")
+    response = requests.post(apikey.communityWebhook,headers=headers,data=values)
+    response.raise_for_status()
+  except HTTPError as http_err:
+    print(f'HTTP error occurred while notifying Mattermost-channel: {http_err}')
+  except Exception as err:
+    print(f'Other error occurred while notifying Mattermost-channel: {err.message} {err.args}')
+  else:
+    print('Success!')
 
 for project in projects:
   now=datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+  print(now)
   current_lock=w.get('https://translate.mattermost.com/api/components/'+projects[project]+'/lock/')
   current_locks[projects[project]]=current_lock
   if (previous_locks[projects[project]]!=current_lock):
+    print("SENT NOTIFICICATION CAUSE LOCK HAS CHANGED")
     notificateChannel(project,current_lock)
     current_locks[projects[project]+"-channel_notified"]=now
   else:
     previous_notification = datetime.strptime(previous_locks[projects[project]+"-channel_notified"], '%Y-%m-%d, %H:%M:%S')
     current_locks[projects[project]+"-channel_notified"]=previous_locks[projects[project]+"-channel_notified"]
-    if (datetime.now()-DT.timedelta(days=7))>previous_notification and current_lock:
+    if (datetime.now()-DT.timedelta(days=7))>previous_notification and current_lock['locked']:
       print("reminder for "+str(projects[project]))
       notificateChannel(project,current_lock)
       current_locks[projects[project]+"-channel_notified"]=now
 json.dump(current_locks,open("locks.txt",'w'))
-
